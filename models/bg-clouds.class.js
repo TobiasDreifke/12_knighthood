fa613e2b75dae873ccbfa394bc531b4d0d8bcfa5
Tobias Dreifke <toobsecret@gmail.com>
2025-10-30 13:27:49 +0100

    clouds load with native size and can be scaled in level_01

--- models/bg-clouds.class.js
+++ models/bg-clouds.class.js
@@ -1,14 +1,35 @@
 class Cloud extends MoveableObject {
-    y = 300;
-    width = 800;
-    height = 400;
     speed = 0.2;
 
-    constructor() {
-        super().loadImage("./01_assets/5_background/layers/4_clouds/clouds_01.png")
-        this.x = Math.random() * 100;
-        this.moveLeft();
+    constructor(imagePath, x, y, scale = 1) {
+        super();
+        this.x = x + Math.random() * 100;
+        this.y = y;
+        this.scale = scale;
+
+        this.loadCloudImage(imagePath);
     }
 
+    loadCloudImage(path) {
+        super.loadImage(path);
 
+        const applyNativeSize = () => {
+            const naturalWidth = this.img.naturalWidth || this.img.width;
+            const naturalHeight = this.img.naturalHeight || this.img.height;
+
+            if (naturalWidth && naturalHeight) {
+                this.width = naturalWidth * this.scale;
+                this.height = naturalHeight * this.scale;
+            }
+        };
+
+        if (this.img.complete) {
+            applyNativeSize();
+        } else {
+            this.img.onload = () => {
+                applyNativeSize();
+                this.img.onload = null;
+            };
+        }
+    }
 }
