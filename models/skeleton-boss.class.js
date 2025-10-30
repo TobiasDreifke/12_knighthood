70971df4ee21f233e5aad39122aed829549ef9dc
Tobias Dreifke <toobsecret@gmail.com>
2025-10-30 15:01:17 +0100

    encounter sound and attack logic

--- models/skeleton-boss.class.js
+++ models/skeleton-boss.class.js
@@ -3,13 +3,23 @@ class SkeletonBoss extends MoveableObject {
     height = 250;
     y = 155;
 
-    offsetLeft = 100;
+    offsetLeft = 90;
     offsetRight = 80;
-    offsetTop = 50;
+    offsetTop = 45;
     offsetBottom = 5;
 
     collidingObject = true;
     debugColor = "green";
+
+    encounterSoundPlayed = false;
+    attackTimers = [];
+
+    IMAGES_IDLE = [
+        "01_assets/4_enemie_boss/2_alert/skeleton_idle_01.png",
+        "01_assets/4_enemie_boss/2_alert/skeleton_idle_02.png",
+        "01_assets/4_enemie_boss/2_alert/skeleton_idle_03.png",
+        "01_assets/4_enemie_boss/2_alert/skeleton_idle_04.png",
+    ];
 
     IMAGES_WALK = [
         "./01_assets/4_enemie_boss/1_walk/skeleton_walk_01.png",
@@ -18,12 +28,6 @@ class SkeletonBoss extends MoveableObject {
         "./01_assets/4_enemie_boss/1_walk/skeleton_walk_04.png"
     ];
 
-    IMAGES_IDLE = [
-        "01_assets/4_enemie_boss/2_alert/skeleton_idle_01.png",
-        "01_assets/4_enemie_boss/2_alert/skeleton_idle_02.png",
-        "01_assets/4_enemie_boss/2_alert/skeleton_idle_03.png",
-        "01_assets/4_enemie_boss/2_alert/skeleton_idle_04.png",
-    ];
     IMAGES_ATTACK = [
         "01_assets/4_enemie_boss/3_attack/skeleton_attack_01.png",
         "01_assets/4_enemie_boss/3_attack/skeleton_attack_02.png",
@@ -34,12 +38,14 @@ class SkeletonBoss extends MoveableObject {
         "01_assets/4_enemie_boss/3_attack/skeleton_attack_07.png",
         "01_assets/4_enemie_boss/3_attack/skeleton_attack_08.png",
     ];
+
     IMAGES_HURT = [
         "01_assets/4_enemie_boss/4_hurt/skeleton_hurt_01.png",
         "01_assets/4_enemie_boss/4_hurt/skeleton_hurt_02.png",
         "01_assets/4_enemie_boss/4_hurt/skeleton_hurt_03.png",
         "01_assets/4_enemie_boss/4_hurt/skeleton_hurt_04.png",
     ];
+
     IMAGES_DEAD = [
         "01_assets/4_enemie_boss/5_dead/skeleton_death_01.png",
         "01_assets/4_enemie_boss/5_dead/skeleton_death_02.png",
@@ -47,118 +53,171 @@ class SkeletonBoss extends MoveableObject {
         "01_assets/4_enemie_boss/5_dead/skeleton_death_04.png",
     ];
 
-
     constructor(player, isHurt = false, isDead = false) {
-        super().loadImage(this.IMAGES_WALK[0]);
-        this.x = 300;
-        this.loadImages(this.IMAGES_WALK);
-        this.loadImages(this.IMAGES_ATTACK);
-        this.loadImages(this.IMAGES_DEAD);
-        this.loadImages(this.IMAGES_HURT);
-
+        super().loadImage(this.IMAGES_IDLE[0]);
         this.player = player;
-        this.speed = 0.0;
+        this.x = 700;
+        this.speed = 0.55;
         this.otherDirection = true;
         this.isHurt = isHurt;
         this.isDead = isDead;
 
         this.isAttacking = false;
-        this.hitboxWidth = 80;          // how far the sword/swing reaches
-        this.hitboxOffsetTop = 50;
-        this.hitboxOffsetBottom = 20;
+        this.attackCooldown = false;
+
+        this.hitboxWidth = 140;
+        this.hitboxOffsetTop = 35;
+        this.hitboxOffsetBottom = 35;
+        this.hitboxOffsetLeft = 35;
+        this.hitboxOffsetRight = 55;
+
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_WALK);
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
 
         if (this.player) this.animation();
     }
 
-    isPlayerInRange(range = 50) {
-        if (!this.player) return false;
-        return Math.abs(this.player.x - this.x) < range;
+    distanceToPlayer() {
+        if (!this.player) return Infinity;
+        const bossCenter = this.x + this.width / 2;
+        const heroCenter = this.player.x + this.player.width / 2;
+        return Math.abs(heroCenter - bossCenter);
     }
 
     animation() {
-
         this.animationInterval = setInterval(() => {
+            if (!this.player) return;
 
             if (this.isDead) {
-                this.playAnimationWithSpeed(this.IMAGES_DEAD, 6); // 6 FPS
+                this.playAnimationWithSpeed(this.IMAGES_DEAD, 6, false);
                 return;
             }
 
             if (this.isHurt) {
-                this.playAnimationWithSpeed(this.IMAGES_HURT, 8); // 8 FPS
+                this.playAnimationWithSpeed(this.IMAGES_HURT, 12, false);
                 this.isHurt = false;
+                this.stopAttackImmediately();
                 return;
             }
 
-            if (this.isPlayerInRange(45)) {
-                if (!this.isAttacking) {
-                    this.isAttacking = true;
-                    this.playAttackAnimationOnce();
-                }
-            } else {
-                this.isAttacking = false;
-                this.walkTowardPlayer();
+            this.otherDirection = this.player.x < this.x;
+
+            if (this.isAttacking) {
+                this.playAnimationWithSpeed(this.IMAGES_ATTACK, 10, false);
+                return;
             }
 
+            if (this.handleAttackLogic()) return;
+            if (this.handleWalkingLogic()) return;
+
+            this.playIdleAnimation();
         }, 1000 / 25);
+    }
+
+    handleAttackLogic() {
+        const attackRange = 140;
+        if (this.distanceToPlayer() > attackRange) return false;
+        if (this.attackCooldown) return true;
+        this.startAttack();
+        return true;
+    }
+
+    handleWalkingLogic() {
+        const walkRange = 540;
+        if (this.distanceToPlayer() > walkRange) return false;
+
+        if (!this.encounterSoundPlayed) {
+            this.encounterSoundPlayed = true;
+            AudioHub.playOne(AudioHub.SKELETON_LAUGHING);
+        }
+
+        this.walkTowardPlayer();
+        return true;
     }
 
     walkTowardPlayer() {
         if (!this.player) return;
 
-        if (this.player.x < this.x) {
-            this.otherDirection = true;
+        if (this.otherDirection) {
             this.moveLeft();
         } else {
-            this.otherDirection = false;
             this.moveRight();
         }
 
         this.playAnimationWithSpeed(this.IMAGES_WALK, 8);
     }
 
-    playAttackAnimationOnce() {
-        if (this.isAttacking) return;
-        this.isAttacking = true;
+    playIdleAnimation() {
+        this.playAnimationWithSpeed(this.IMAGES_IDLE, 6);
+    }
 
-        const fps = 5;
+    startAttack() {
+        this.clearAttackTimers();
+        this.isAttacking = true;
+        this.attackCooldown = true;
+        this.frameIndex = 0;
+        this.lastFrameTime = 0;
+
+        const fps = 10;
         const frameDuration = 1000 / fps;
+        const hitFrame = 4;
         const totalDuration = this.IMAGES_ATTACK.length * frameDuration;
 
-        this.playAnimationWithSpeed(this.IMAGES_ATTACK, fps);
+        AudioHub.playOne(AudioHub.SKELETON_ATTACK);
 
-        // Sword connects around frame 4 → ~800 ms into attack
-        const hitTime = 4 * frameDuration;
-        setTimeout(() => this.attackPlayer(), hitTime);
-
-        // Reset attack cycle after animation ends
-        setTimeout(() => this.isAttacking = false, totalDuration);
+        this.attackTimers.push(
+            setTimeout(() => this.attackPlayer(), hitFrame * frameDuration),
+            setTimeout(() => {
+                this.isAttacking = false;
+                this.attackTimers.push(
+                    setTimeout(() => this.attackCooldown = false, 350)
+                );
+            }, totalDuration)
+        );
     }
-
 
     attackPlayer() {
-        console.log(`[${this.constructor.name}] attacks [HERO]`);
+        if (!this.player) return;
 
-        // Slight delay to align with sword swing frame
-        setTimeout(() => {
-            const hitbox = this.getHitbox();
-            const playerHurtbox = this.player.getHurtbox();
+        const hitbox = this.getHitbox();
+        const playerHurtbox = this.player.getHurtbox();
 
-            const isHit =
-                hitbox.right > playerHurtbox.left &&
-                hitbox.left < playerHurtbox.right &&
-                hitbox.bottom > playerHurtbox.top &&
-                hitbox.top < playerHurtbox.bottom;
+        const isHit =
+            hitbox.right > playerHurtbox.left &&
+            hitbox.left < playerHurtbox.right &&
+            hitbox.bottom > playerHurtbox.top &&
+            hitbox.top < playerHurtbox.bottom;
 
-            if (isHit) {
-                console.log("Skeleton hit HERO!");
-                this.player.hit();
-            }
-        }, 400); // Adjust timing to match swing frame
+        if (isHit) {
+            this.player.hit();
+        }
     }
 
+    stopAttackImmediately() {
+        this.clearAttackTimers();
+        this.isAttacking = false;
+        this.attackCooldown = true;
+        this.frameIndex = 0;
+        this.lastFrameTime = 0;
+        this.attackTimers.push(
+            setTimeout(() => this.attackCooldown = false, 400)
+        );
+    }
 
+    clearAttackTimers() {
+        this.attackTimers.forEach(clearTimeout);
+        this.attackTimers = [];
+    }
 
-
-
+    hit(amount = this.damageOnCollision) {
+        super.hit(amount);
+        if (!this.isDead) {
+            this.stopAttackImmediately();
+        } else {
+            this.clearAttackTimers();
+        }
+    }
 }
